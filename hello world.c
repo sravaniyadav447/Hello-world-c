5c5bf04805c526b8a0ee60d2935492c164ed5520
sravaniyadav447 <131240526+sravaniyadav447@users.noreply.github.com>
2023-09-22 18:39:34 +0530

    Add files via upload

---
+++ hello world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int s;
+    s="Welcome to C programming.";
+    printf("Hello, World!");
+    printf("\nWelcome to C programming.",s);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
